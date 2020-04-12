eb751b456fab383ecf3b0878dc708459b57da31e
jediHub <jediHub>
2020-04-13 03:34:45 +0530

    homepage Added and syled

---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './homepage.styles.scss';
+
+export const HomePage = () => {
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className='menu-item'>
+                    <div className='content'>
+                        <div className='title'> HAT</div>
+                        <div className='subtitle'>Shop Now</div>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <div className='title'> JACKETS</div>
+                        <div className='subtitle'>Shop Now</div>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <div className='title'> SNEAKERS</div>
+                        <div className='subtitle'>Shop Now</div>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <div className='title'> WOMENS</div>
+                        <div className='subtitle'>Shop Now</div>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <div className='title'> MENS</div>
+                        <div className='subtitle'>Shop Now</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
